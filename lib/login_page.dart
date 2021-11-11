c5409355c94e3c562dd18515478049f1725ca3d2
Kevin <kevin.jakuszkowiak@gmail.com>
2021-11-10 18:55:00 -0600

    base login page

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -1,6 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:poke_team_planner/validator.dart';
+
+import 'fire_auth.dart';
+
 class LoginPage extends StatelessWidget {
   final _formKey = GlobalKey<FormState>();
   final _emailTextController = TextEditingController();
@@ -12,6 +16,7 @@ class LoginPage extends StatelessWidget {
     FirebaseApp firebaseApp = await Firebase.initializeApp();
     return firebaseApp;
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,21 +39,28 @@ class LoginPage extends StatelessWidget {
                         children: <Widget>[
                           Center(
                             child: TextFormField(
-                              decoration:
-                              const InputDecoration.collapsed(hintText: 'Email'),
+                              decoration: const InputDecoration.collapsed(
+                                  hintText: 'Email',
+                              border: InputBorder.none),
+                              style: const TextStyle(
+                                  fontSize: 25),
                               controller: _emailTextController,
                               focusNode: _focusEmail,
-                              validator: (value) => Validator.validateEmail(email: value),
+                              validator: (value) =>
+                                  Validator.validateEmail(email: value),
                             ),
                           ),
-                          SizedBox(height: 8.0),
+                          SizedBox(height: 20.0),
                           TextFormField(
-                            decoration:
-                            const InputDecoration.collapsed(hintText: 'Password'),
+                            decoration: const InputDecoration.collapsed(
+                                hintText: 'Password'),
+                            style: const TextStyle(
+                                fontSize: 25),
                             controller: _passwordTextController,
                             focusNode: _focusPassword,
                             obscureText: true,
-                            validator: (value) => Validator.validatePassword(password: value),
+                            validator: (value) =>
+                                Validator.validatePassword(password: value),
                           ),
                         ],
                       ),
@@ -59,6 +71,46 @@ class LoginPage extends StatelessWidget {
                   );
                 },
               ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () async {
+                        if (_formKey.currentState!.validate()) {
+                          User? user = await FireAuth.signInUsingEmailPassword(
+                            email: _emailTextController.text,
+                            password: _passwordTextController.text, context: context,
+                          );
+                          // if (user != null) {
+                          //   Navigator.of(context)
+                          //       .pushReplacement(
+                          //     MaterialPageRoute(builder: (context) => ProfilePage(user: user)),
+                          //   );
+                          // }
+                        }
+                      },
+                      child: Text(
+                        'Sign In',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Navigator.of(context).push(
+                        //   MaterialPageRoute(builder: (context) => RegisterPage()),
+                        // );
+                      },
+                      child: Text(
+                        'Register',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
         ),
